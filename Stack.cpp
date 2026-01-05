4128571d9ceb2b25e5f8b1617d762feefce1da7e
Julianna Della Selva <141692914+Julie-23@users.noreply.github.com>
2026-01-05 17:18:55 -0500

    Implement Stack class methods using LinkedList

--- Stack.cpp
+++ Stack.cpp
@@ -1,1 +1,32 @@
 //Stack.cpp
+#include "Stack.hpp"
+#include <iostream>
+
+Stack::Stack(){
+    LinkedList list;
+}
+
+void Stack::push(int value){
+    list.insertFront(value);
+}
+
+int Stack::pop(){
+    if (list.isEmpty()){
+        std::cerr<<"Error: Cannot remove from empty Stack";
+    } else{
+        int value = list.front();
+        list.removeFront();
+        return value;
+    }
+}
+
+int Stack::peek() const{
+    if (list.isEmpty()) {
+        std::cerr<<"Error: Stack is empty";
+    } else{
+        return list.front();
+    }
+}
+bool Stack::isEmpty() const{
+    return list.isEmpty();
+}
