c670efedd07ac75682083ef6343b33cbc6a44ac5
Julianna Della Selva <141692914+Julie-23@users.noreply.github.com>
2026-01-05 17:18:06 -0500

    Add header guards and new methods to LinkedList
    Added header guards and new member functions to LinkedList class.

--- LinkedList.hpp
+++ LinkedList.hpp
@@ -1,6 +1,8 @@
+//linkedList.hpp
 #include <string>
 #include "Node.hpp"
-
+#ifndef LINKEDLIST_HPP
+#define LINKEDLIST_HPP
 class LinkedList{
     private:
         Node* head;
@@ -11,5 +13,9 @@ class LinkedList{
         void remove(int value);
         bool search(int value);
         void printList();
+        bool isEmpty() const;
+        void removeFront();
+        int front() const;
     
-}
+};
+#endif
